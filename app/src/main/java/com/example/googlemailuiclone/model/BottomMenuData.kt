ce544e1d8f9a80f3c3d451ae6bad9ee3c27d9f04
Lumbani Makumbi <55507829+makumbilumbani@users.noreply.github.com>
2023-06-07 14:20:04 +0200

    initial files

---
+++ app/src/main/java/com/example/googlemailuiclone/model/BottomMenuData.kt
@@ -0,0 +1,19 @@
+package com.example.googlemailuiclone.model
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.VideoCall
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomMenuData (
+    val icon: ImageVector,
+    val title: String){
+
+    object Mail : BottomMenuData(
+        icon = Icons.Outlined.Mail, "Mail"
+    )
+    object Meet : BottomMenuData(
+        icon = Icons.Outlined.VideoCall, "Meet"
+    )
+
+}
